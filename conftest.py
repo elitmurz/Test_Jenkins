f43f1d003d9e9a0c006dc195da3766cfae13ac48
elitmurz <den4iktanist@gmail.com>
2025-11-18 16:46:35 +0300

    fix: correct conftest configuration and browser setup

--- conftest.py
+++ conftest.py
@@ -1,13 +1,13 @@
 import pytest
-
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
+import allure
+from allure_commons.types import AttachmentType
 
-from utils import attach
 
 @pytest.fixture(scope='function')
-def setup_browser(request):
+def setup_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -18,10 +18,44 @@ def setup_browser(request):
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
+
+    browser.config.driver = driver
+    browser.config.base_url = "https://demoqa.com"
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.timeout = 10.0
+
+    yield browser
+
+    # Добавляем скриншот при падении
+    add_screenshot(browser)
+    add_html(browser)
+
+    browser.quit()
+
+
+def add_screenshot(browser):
+    png = browser.driver.get_screenshot_as_png()
+    allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
+
+
+def add_html(browser):
+    html = browser.driver.page_source
+    allure.attach(body=html, name='page_source', attachment_type=AttachmentType.HTML, extension='.html')
+
+
+def add_video(browser):
+    video_url = f"https://selenoid.autotests.cloud/video/{browser.driver.session_id}.mp4"
+    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
+           + video_url \
+           + "' type='video/mp4'></video></body></html>"
+    allure.attach(body=html, name='video_' + browser.driver.session_id, attachment_type=AttachmentType.HTML,
+                  extension='.html')
 
     browser = Browser(Config(driver))
     yield browser
